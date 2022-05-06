fcedc062f8a6fe1ed1944c78b5d87a5ee7577ed4
Vladimir <pestov.web@yandex.ru>
2022-05-06 14:54:46 +1000

    fix: logo link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Logo() {
   return (
     <div className="logo">
       <h1>
-        <Link to="/" className="logo__text">
+        <a href="https://linzer.su/" className="logo__text">
           <span className="logo__text_type_green">lin</span>zer
-        </Link>
+        </a>
       </h1>
     </div>
   );
